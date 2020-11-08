f1c4ce47c7dd98dee8624218af3ef8b8fb08b87f
David <davidyuen0305@gmail.com>
2020-11-07 21:15:28 -0800

    installed firebase to project.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Sidebar.css';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/CreateSharp';
@@ -13,9 +13,21 @@ import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import SidebarOption from './SidebarOption';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
+import db from './firebase';
 
 function Sidebar() {
     const [channels, setChannels] = useState([])
+
+    useEffect(() => {
+        db.collection('rooms').onSnapshot(snapshot => (
+            setChannels(
+                snapshot.docs.map(doc=>({
+                    id: doc.id,
+                    name: doc.data().name,
+                }))
+            )
+        ))
+    }, [])
     return (
         <div className='sidebar'>
             <div className ='sidebar__header'>
@@ -41,7 +53,9 @@ function Sidebar() {
             <hr />
             <SidebarOption Icon={ AddIcon } title='Add Channel' />
             
-
+            {channels.map(channel => (
+                <SidebarOption title={channel.name} id= {channel.id} />
+            ))}
         </div>
     )
 }
